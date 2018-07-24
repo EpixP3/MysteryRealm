e47eae34e926725a7ea666e7cd57b38b91f56e40
Fapio <straka.filip@gmail.com>
2018-07-24 03:11:44 +0200

    Added ui images

---
+++ Mystery Realm/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public GameObject menuMenu, playMenu, optionsMenu, tutorial;
+    public Image blackBackgorund, loadingOverlay;
+    
+	void Start () {
+        tutorial.SetActive(false);
+        blackBackgorund.CrossFadeAlpha(0f, 0f, true);
+        loadingOverlay.CrossFadeAlpha(0f, 0f, true);
+        loadingOverlay.gameObject.SetActive(false);
+    }
+	
+	void Update () {
+		
+	}
+
+    public void GoToPlayMenu()
+    {
+        if (PlayerPrefs.GetInt("FirstPlay") == 0)
+        {
+            //First play --> Tutorial
+            tutorial.SetActive(true);
+            blackBackgorund.CrossFadeAlpha(1f, 1f, true);
+        }
+        else
+        {
+            menuMenu.SetActive(false);
+            playMenu.SetActive(true);
+        }
+    }
+    public void GoToOptionsMenu()
+    {
+        menuMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+    public void GoToMenuMenu()
+    {
+        menuMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+        playMenu.SetActive(false);
+    }
+    public void TutorialOkBtn()
+    {
+        loadingOverlay.gameObject.SetActive(true);
+        loadingOverlay.CrossFadeAlpha(0f, 0f, true);
+        loadingOverlay.CrossFadeAlpha(1f, 0.5f, true);
+        StartCoroutine(LoadNewLevel());
+    }
+    IEnumerator LoadNewLevel()
+    {
+        yield return new WaitForSeconds(0.8f);
+        SceneManager.LoadScene("Tutorial", LoadSceneMode.Single);
+    }
+}
---
+++ Mystery Realm/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialScript : MonoBehaviour {
+
+    public Image loadingOverlay;
+
+	void Start () {
+        loadingOverlay.gameObject.SetActive(true);
+        StartCoroutine(CrossfadeLoading());
+	}
+	
+	void Update () {
+		
+	}
+    IEnumerator CrossfadeLoading()
+    {
+        yield return new WaitForSeconds(0.8f);
+        loadingOverlay.CrossFadeAlpha(0f, 0.5f, true);
+    }
+}
